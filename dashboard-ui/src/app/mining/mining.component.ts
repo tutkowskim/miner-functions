5db1279fcdf6d79a2fd79e70a7088b9cef7d26ac
Mark Tutkowski <tutkows1@gmail.com>
2023-02-23 21:10:14 -0600

    Show current month's profits

--- dashboard-ui/src/app/mining/mining.component.ts
+++ dashboard-ui/src/app/mining/mining.component.ts
@@ -10,19 +10,27 @@ import { MiningPayout, MiningPowerUsage, MiningService } from './mining.service'
 export class MiningComponent {
   public readonly miningPayouts$;
   public readonly dailyMiningPowerUsage$;
+  public readonly currentCost$;
+  public readonly currentIncome$;
+  public readonly currentProfit$;
 
   public readonly miningPayoutsColumDefinitions: ColumnDefinition[];
   public readonly dailyMiningPowerUsageColumDefinitions: ColumnDefinition[];
 
   constructor(private miningService: MiningService) {
     this.miningPayouts$ = this.miningService.miningPayouts$;
+    this.currentCost$ = this.miningService.currentCost$;
     this.dailyMiningPowerUsage$ = this.miningService.dailyMiningPowerUsage$;
+    this.currentIncome$ = this.miningService.currentIncome$;
+    this.currentProfit$ = this.miningService.currentProfit$;
+
     this.miningPayoutsColumDefinitions = [
       { displayName: 'Timestamp', fetchColumnValue: (item: MiningPayout) => new Date(item.timestamp).toISOString() },
       { displayName: 'Asset', fetchColumnValue: (item: MiningPayout) => item.asset_acquired },
       { displayName: 'Quantity', fetchColumnValue: (item: MiningPayout) => String(item.quantity_acquired.toPrecision(6)) },
       { displayName: 'Income ($)', fetchColumnValue: (item: MiningPayout) => String(item.income.toFixed(2)) },
     ];
+    
     this.dailyMiningPowerUsageColumDefinitions = [
       { displayName: 'Date', fetchColumnValue: (item: MiningPowerUsage) => new Date(item.date).toDateString() },
       { displayName: 'Rig', fetchColumnValue: (item: MiningPowerUsage) => item.rig },
--- dashboard-ui/src/app/mining/mining.service.ts
+++ dashboard-ui/src/app/mining/mining.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 export interface MiningPayout {
   id: string;
@@ -22,15 +22,31 @@ export interface MiningPowerUsage {
   usd_per_kw: number;
 }
 
+export interface MonthlyMiningStatistic {
+  id: string;
+  month: string;
+  cost: number;
+  income: number;
+  profit: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class MiningService {
   public readonly miningPayouts$: Observable<MiningPayout[]>;
   public readonly dailyMiningPowerUsage$: Observable<MiningPowerUsage[]>;
+  public readonly monthlyMiningStatistics$: Observable<MonthlyMiningStatistic[]>;
+  public readonly currentCost$: Observable<number>;
+  public readonly currentIncome$: Observable<number>;
+  public readonly currentProfit$: Observable<number>;
 
   constructor(http: HttpClient) {
     this.miningPayouts$ = http.get<MiningPayout[]>('/api/FetchMiningPayouts');
     this.dailyMiningPowerUsage$ = http.get<MiningPowerUsage[]>('/api/FetchMiningPowerUsage');
+    this.monthlyMiningStatistics$ = http.get<MonthlyMiningStatistic[]>('/api/FetchMonthlyStatistics');
+    this.currentCost$ = this.monthlyMiningStatistics$.pipe(map(value => value[0].cost));
+    this.currentIncome$ = this.monthlyMiningStatistics$.pipe(map(value => value[0].income));
+    this.currentProfit$ = this.monthlyMiningStatistics$.pipe(map(value => value[0].profit));
   }
 }
